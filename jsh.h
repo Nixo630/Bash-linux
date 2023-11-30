cb7d16c9439a1439dd1a2fa8f4c109b5711099bc
Albertu17 <tomasialbert3@gmail.com>
2023-11-30 13:22:43 +0100

    nombre illimité mots remise

--- jsh.h
+++ jsh.h
@@ -4,10 +4,10 @@ int question_mark();
 void callRightCommand(char**, unsigned);
 void print_path();
 void cd(char*);
-void exit_jsh();
+void exit_jsh(int);
 int main(int argc, char** argv);
 void main_loop();
-void reset(char** args);
+void reset(char** args, size_t);
 
 bool running;
 int lastReturn;
