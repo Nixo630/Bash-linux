76e369c3b1369b804abe45d4907085fbca0a675c
ESPANET nicolas <nicolas.espanet7@gmail.com>
2023-12-21 23:38:12 +0100

    Update toolbox_jsh.c

--- toolbox_jsh.c
+++ toolbox_jsh.c
@@ -16,10 +16,31 @@ int convert_str_to_int (char* string) {
     char** tmp = malloc(sizeof(char)*50);
     int int_args = strtol(string,tmp,10);//base 10 and we store invalids arguments in tmp
     if ((strcmp(tmp[0],"") != 0 && strlen(tmp[0]) > 0) || int_args == LONG_MIN || int_args == LONG_MAX) {//we check the second argument doesn't contain some chars
-        fprintf(stderr,"Exit takes a normal integer as argument\n");
+        if (tmp[0][0] == '%') {
+            char* string2 = malloc(sizeof(char)*strlen(string)-1);
+            for (int i = 0; i < strlen(string)-1; i++) {
+                *(string2+i) = *(string+i+1);
+            }
+            free(tmp);
+            char** tmp2 = malloc(sizeof(char)*50);
+            string2[strlen(string)-1] = '\0';
+            int_args = strtol(string2,tmp2,10);
+            if ((strcmp(tmp2[0],"") != 0 && strlen(tmp2[0]) > 0) || int_args == LONG_MIN || int_args == LONG_MAX) {
+                free(tmp2);
+                free(string2);
+                return INT_MIN;
+            }
+            free(tmp2);
+            free(string2);
+            int_args = int_args*(-1);
+            printf("%d\n",int_args);
+            return int_args;
+        }
+        free(tmp);
         return INT_MIN;
     }
     free(tmp);
+    printf("%d\n",int_args);
     return int_args;
 }
 
