35d57f554306aa8068f2a268ad300a78cc0c7355
TOMASI albert <albert.tomasi@etu.u-paris.fr>
2024-01-17 23:45:46 +0100

    Revert "essai nouveau module jobs"
    This reverts commit f08d1007aefb9596ed012e966db775144b4456d9

--- jsh.h
+++ jsh.h
@@ -1,5 +1,14 @@
 #include <stdbool.h>
-#include "jobs_jsh.h"
+
+// Structures
+struct Job {//maximum de 40 jobs simultanément
+    int nJob;
+    int pid;
+    char* state;
+    char* command_name;
+    char* ground;
+};
+typedef struct Job Job;
 
 // Fonctions de commande
 char* pwd();
@@ -7,21 +16,30 @@ int cd(char* pathname);
 int exit_jsh(int val);
 void print_lastReturn();
 int external_command(Command* command, int pipe_out[2]);
+void print_job(Job job);
+void print_jobs(pid_t job,bool isJob,bool tHyphen);
+void removeJob (int n);
+int killJob (char* sig, char* pid);
 
 // Fonctions auxiliaires
 int main(int argc, char** argv);
 void main_loop();
+void cmd_background(Command * command);
 void execute_command(Command* command, int pipe_out[2]);
 int apply_redirections(Command* command, int pipe_in[2], int pipe_out[2]);
 int callRightCommand(Command* command);
 bool correct_nbArgs(char**, unsigned, unsigned);
 char* getPrompt();
+void create_job(char * command_name, char *status, pid_t pid, char * ground);
+int bg(int job_num);
+int fg(int job_num);
 
 // Variables globales
 bool running;
 int lastReturn;
 char* current_folder;
 char* previous_folder;
-extern int nbJobs;
-extern Job* l_jobs;
+int nbJobs;
+Job* l_jobs;
 struct sigaction sa;
+int nTimesPrintStop;
